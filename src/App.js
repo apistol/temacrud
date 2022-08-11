83186eecba108f182f3aef79f52f53456c4e547d
EXT_API97 <EXT_API97@open-groupe.com>
2022-08-11 15:14:40 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      name: "alexandru"
+    }
+    
+  }
+
+
+  changeName() {
+    return this.setState({
+      name:"ALexandru Pistol"
+    })
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.name}<br/>
+        <button onClick={ this.changeName}> Change name </button>
+      </div>
+    );
+  }
+
 }
 
 export default App;
